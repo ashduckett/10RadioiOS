1ed63164c6e10a4177d449ab66c8a482b66459ed
Ash Duckett <ash@ashs-imac.home>
2019-09-22 19:30:39 +0100

    Add support for playing when phone is locked

--- 10Radio/AppDelegate.swift
+++ 10Radio/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import GoogleMobileAds
+import AVKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GADMobileAds.sharedInstance().start(completionHandler: nil)
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            
+        }
         return true
     }
 
