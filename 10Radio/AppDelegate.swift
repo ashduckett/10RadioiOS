eea0556ed25fcba462ee7297d727e916957405ba
Ash Duckett <ash@ashs-imac.home>
2020-05-23 18:43:22 +0100

    Add button and splash screen

--- 10Radio/AppDelegate.swift
+++ 10Radio/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         
+        // This was necessary along with changes in plist.
         do {
             try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
             try AVAudioSession.sharedInstance().setActive(true)
--- 10Radio/ViewController.swift
+++ 10Radio/ViewController.swift
@@ -11,53 +11,39 @@ import GoogleMobileAds
 import AVKit
 
 class ViewController: UIViewController, GADBannerViewDelegate {
-
+    
+    var player: AVPlayer?
+    var srcUrl: URL?
+    var playerItem: AVPlayerItem!
+    
+    @IBOutlet weak var playButton: UIButton!
+  
+    func startRadio() {
+        srcUrl = URL.init(string: "http://live.canstream.co.uk:8000/10radio.mp3.m3u")
+        playerItem = AVPlayerItem(url: srcUrl!)
+        player = AVPlayer(playerItem: playerItem)
+        player!.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        var adBannerView: GADBannerView?
-        var player: AVPlayer?
-        let request = GADRequest()
-//        request.testDevices = [(kGADSimulatorID as! String)]
-        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [(kGADSimulatorID as! String)]
-        adBannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
-        // adBannerView?.adUnitID = "ca-app-pub-4647277951971448/7305051200"
-        adBannerView?.adUnitID = "ca-app-pub-3940256099942544/2934735716"
-        adBannerView?.delegate = self
-        adBannerView?.rootViewController = self
-        
-        adBannerView?.load(GADRequest())
-        
-        
-        
-        view.addSubview(adBannerView!)
-
-        adBannerView?.translatesAutoresizingMaskIntoConstraints = false
-
-
-        view.addConstraint(NSLayoutConstraint(item: adBannerView!, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: 0))
-        
-        view.addConstraint(NSLayoutConstraint(item: adBannerView!, attribute: NSLayoutConstraint.Attribute.bottom, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1, constant: 0))
-        
-        
-        
-        
-        
-        
-        
-        let url = URL.init(string: "http://live.canstream.co.uk:8000/10radio.mp3.m3u")
-        let playerItem: AVPlayerItem = AVPlayerItem(url: url!)
-        player = AVPlayer(playerItem: playerItem)
-        
-        let playerLayer = AVPlayerLayer(player: player!)
-        
-        
-        
+        let playerLayer = AVPlayerLayer(player: player)
         playerLayer.frame = CGRect(x: 0, y: 0, width: 10, height: 10)
         self.view.layer.addSublayer(playerLayer)
-        player?.play()
+        startRadio()
     }
 
-
+    @IBAction func playButtonClicked(_ sender: Any) {
+        if let _ = player {
+            self.player!.pause()
+            self.player = nil
+            let playImage = UIImage(systemName: "play.fill")
+            playButton.setBackgroundImage(playImage, for: .normal)
+        } else {
+            startRadio()
+            let playImage = UIImage(systemName: "pause.fill")
+            playButton.setBackgroundImage(playImage, for: .normal)
+        }
+    }
 }
 
